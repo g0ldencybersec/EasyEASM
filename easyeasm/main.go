16d10f6db1e228163defba41595f7588e0bcbdb3
oliviagallucci <oag1356@rit.edu>
2023-08-09 15:39:14 -0700

    commented

--- easyeasm/main.go
+++ easyeasm/main.go
@@ -12,11 +12,17 @@ import (
 )
 
 func main() {
+	// install required tools
 	utils.InstallTools()
-	// Parse the configuraton file
+
+	// print a banner
+	banner := "\x1b[36m****************\n\nEASY EASM\n\n***************\x1b[0m\n"
+	fmt.Println(banner)
+
+	// parse the configuration file
 	cfg := configparser.ParseConfig()
 
-	// Check for previous run file
+	// check for previous run file
 	var prevRun bool
 	if _, err := os.Stat("EasyEASM.csv"); err == nil {
 		fmt.Println("Found data from previous run!")
@@ -30,20 +36,30 @@ func main() {
 		prevRun = false
 	}
 
-	// Fast run. This is passive enumeration only
+	// check the run type specified in the config and perform actions accordingly
 	if strings.ToLower(cfg.RunConfig.RunType) == "fast" {
+		// fast run: passive enumeration only
+
+		// create a PassiveRunner instance
 		Runner := passive.PassiveRunner{
 			SeedDomains: cfg.RunConfig.Domains,
 		}
+
+		// run passive enumeration and get the results
 		passiveResults := Runner.RunPassiveEnum()
 
+		// remove duplicate subdomains
 		Runner.Subdomains = utils.RemoveDuplicates(passiveResults)
 		Runner.Results = len(Runner.Subdomains)
 
-		fmt.Printf("Found %d subdomains\n\n", Runner.Results)
+		fmt.Printf("\x1b[31mFound %d subdomains\n\n\x1b[0m", Runner.Results)
 		fmt.Println(Runner.Subdomains)
 		fmt.Println("Checking which domains are live and generating assets csv...")
+
+		// run Httpx to check live domains
 		Runner.RunHttpx()
+
+		// notify about new domains if prevRun is true
 		if prevRun && strings.Contains(cfg.RunConfig.SlackWebhook, "https") {
 			utils.NotifyNewDomainsSlack(Runner.Subdomains, cfg.RunConfig.SlackWebhook)
 			os.Remove("old_EasyEASM.csv")
@@ -52,16 +68,19 @@ func main() {
 			os.Remove("old_EasyEASM.csv")
 		}
 	} else if strings.ToLower(cfg.RunConfig.RunType) == "complete" {
-		// PASSIVE
+		// complete run: passive and active enumeration
+
+		// passive enumeration
 		PassiveRunner := passive.PassiveRunner{
 			SeedDomains: cfg.RunConfig.Domains,
 		}
 		passiveResults := PassiveRunner.RunPassiveEnum()
 
+		// remove duplicate subdomains
 		PassiveRunner.Subdomains = utils.RemoveDuplicates(passiveResults)
 		PassiveRunner.Results = len(PassiveRunner.Subdomains)
 
-		//ACTIVE
+		// active enumeration
 		ActiveRunner := active.ActiveRunner{
 			SeedDomains: cfg.RunConfig.Domains,
 		}
@@ -70,18 +89,19 @@ func main() {
 
 		ActiveRunner.Subdomains = utils.RemoveDuplicates(activeResults)
 
-		//ALTERX
+		// permutation scan
 		permutationResults := ActiveRunner.RunPermutationScan(cfg.RunConfig.ActiveThreads)
 		ActiveRunner.Subdomains = append(ActiveRunner.Subdomains, permutationResults...)
-
 		ActiveRunner.Subdomains = utils.RemoveDuplicates(ActiveRunner.Subdomains)
 		ActiveRunner.Results = len(ActiveRunner.Subdomains)
 
-		//HTTPX
+		// httpx scan
 		fmt.Printf("Found %d subdomains\n\n", ActiveRunner.Results)
 		fmt.Println(ActiveRunner.Subdomains)
 		fmt.Println("Checking which domains are live and generating assets csv...")
 		ActiveRunner.RunHttpx()
+
+		// notify about new domains if prevRun is true
 		if prevRun && strings.Contains(cfg.RunConfig.SlackWebhook, "https") {
 			utils.NotifyNewDomainsSlack(ActiveRunner.Subdomains, cfg.RunConfig.SlackWebhook)
 			os.Remove("old_EasyEASM.csv")
@@ -90,7 +110,7 @@ func main() {
 			os.Remove("old_EasyEASM.csv")
 		}
 	} else {
+		// invalid run mode specified
 		panic("Please pick a valid run mode and add it to your config.yml file! You can set runType to either 'fast' or 'complete'")
 	}
-
 }
